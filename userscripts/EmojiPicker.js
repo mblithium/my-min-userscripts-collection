5bd100768b96ba35090fcaa6166195019c0d7c04
mblithium <matheusmcy1@gmail.com>
2022-11-08 20:12:57 -0300

    Added EmojiPicker userscript initial release

---
+++ userscripts/EmojiPicker.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         EmojiPicker Menu
+// @version      0.1
+// @description  search, select, copy and past emojis.
+// @author       mblithium
+// @match        *
+// @run-at       context-menu
+// ==/UserScript==
+
+(
+    function() { 
+        'use strict'; 
+    }
+)();
+
+/* For more information: https://github.com/minbrowser/min/wiki/userscripts */
